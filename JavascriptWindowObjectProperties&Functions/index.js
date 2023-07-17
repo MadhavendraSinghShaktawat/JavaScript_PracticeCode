3de47b64ce80e0dc5f16af2ab680ff2bfaf7c6e1
Madhavendra Singh Shaktawat <112866694+MadhavendraSinghShaktawat@users.noreply.github.com>
2023-07-17 14:47:01 +0530

    forth commit

---
+++ JavascriptWindowObjectProperties&Functions/index.js
@@ -0,0 +1,22 @@
+console.log("Yep! its working");
+
+// ---------- window object properties ---------- 
+let a = document;
+let b = window.innerWidth;
+b = window.innerHeight;
+
+// history.go(-1);    this is to go to prebious page/ back
+// scroolX - gives u scrool value in x direction
+// scroolY - gives u scrool value in y direction
+
+// console.log(a);
+// console.log(b);
+
+ 
+// ---------- Windows Object Methods ---------- 
+let c = document;
+window.alert("Alert");
+window.prompt("Enter ur login id");
+window.confirm("Are u sure u want to continue");
+
+// window.closw()  this will close ur window
