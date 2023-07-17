8242c927ab8ed071501594f8e8fe1b17cf1b362e
Madhavendra Singh Shaktawat <112866694+MadhavendraSinghShaktawat@users.noreply.github.com>
2023-07-17 15:59:43 +0530

    fourth commit

---
+++ Basics_Of_DOM/index.js
@@ -0,0 +1,27 @@
+console.log("Yep! its working");
+let a = document;
+console.log(a);
+
+a = a.all;
+console.log(a);
+
+a = document.body;
+console.log(a);
+
+a = document.documentElement;
+console.log(a);
+
+a = document.head;
+console.log(a);
+
+a = document.anchors;
+console.log(a);
+
+a = document.forms;
+console.log(a);
+
+a = document.URL;
+console.log(a);
+
+a = document.title;
+console.log(a);
